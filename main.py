3c80d03e186b5d749d0424ed63e86116fa54d038
Alec Freedman <afreedman1@ufl.edu>
2024-04-10 18:33:44 -0400

    new stuff

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-output = ''
+def encode(code):
+    output = ''
+    for i in range(len(code)):
+        if int(text[i]) + 3 > 10:
+            output += str(int(code[i]) + 3 - 10)
+        else:
+            output += str(int(code[i]) + 3)
+    return output
+
+
+
 running = True
 while running:
-    text = input("enter 8 digits: ")
-    if text.isdigit() and len(text) == 8:
-        for i in range(len(text)):
-            if int(text[i])+3 > 10:
-                output += str(int(text[i]) + 3-10)
-            else:
-                output += str(int(text[i])+3)
+    print("Welcome to the Encoder Program!")
+    print("1. Encode a number")
+    print("2. Decode a number")
+    print("3. Exit")
+    i = input("Enter your choice: ")
+    if i == "1":
+        num = input("Enter an 8 digit number: ")
+        print(encode(num))
+    elif i == "2":
+        pass
+    elif i == "3":
         running = False
-print(output)
+        print("Thank you for using the encoder program!")
+    else:
+        print("Invalid Input")
+#spaghetti
